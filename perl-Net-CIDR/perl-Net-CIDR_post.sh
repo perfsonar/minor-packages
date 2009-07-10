86fb5d495c527b6b8b2e3eed30989ff752c58d7d
zurawski <zurawski@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2009-07-10 03:56:59 +0000

    Minor change to support 64 bit
    -jason
    
    
    
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/branches/RELEASE_3.1@2909 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- perl-Net-CIDR/perl-Net-CIDR_post.sh
+++ perl-Net-CIDR/perl-Net-CIDR_post.sh
@@ -21,7 +21,7 @@ else
         ln -s $installprefix/$defaultstyle/Net/CIDR.pm $installprefix/$installstyle/vendor_perl/$version/Net
         if [ "$installstyle" != "$defaultstyle" ]
         then
-            ln -s $installprefix/$defaultstyle/Net.pm $installprefix/$defaultstyle/vendor_perl/$version/Net
+            ln -s $installprefix/$defaultstyle/Net/CIDR.pm $installprefix/$defaultstyle/vendor_perl/$version/Net
         fi
     fi
 fi
