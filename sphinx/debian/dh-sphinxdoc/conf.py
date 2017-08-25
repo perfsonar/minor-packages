6309a8fd6fff11a30ae592dba6ad37a471a267e3
Valentin Vidic <Valentin.Vidic@CARNet.hr>
2017-08-25 15:56:09 +0200

    Revert "sphinx: backport for pysnmp4 on wheezy"
    sphinx build disabled in pysnmp4

--- sphinx/debian/dh-sphinxdoc/conf.py
+++
@@ -1,3 +0,0 @@
-master_doc = 'empty'
-html_theme = 'classic'
-html_theme_options = {'collapsiblesidebar': True}
--- sphinx/debian/dh-sphinxdoc/sphinxdoc.pm
+++
@@ -1,8 +0,0 @@
-use warnings;
-use strict;
-
-use Debian::Debhelper::Dh_Lib;
-
-insert_after('dh_installdocs', 'dh_sphinxdoc');
-
-1;
