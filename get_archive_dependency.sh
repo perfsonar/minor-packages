82cd75ee07b016f006e16c73bc446db9ac137ab7
Daniel Neto <daniel.jsilvan@gmail.com>
2022-02-20 00:52:13 -0300

    Adding files to build opensearch rpms

---
+++ get_archive_dependency.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+  then
+    echo "Illegal number of parameters"
+    echo "Usage: $0 PACKAGE DEB/RPM"
+    exit 1
+fi
+
+package=$1
+version=$2
+
+if [ "$version" != "DEB" ] && [ "$version" != "RPM" ]; then
+    echo "Unknown OS version. Use DEB or RPM"
+    exit 1
+fi
+
+if [ "$package" = "opensearch" ] || [ "$package" = "dashboards" ]; then
+    docker-compose -p minor-packages -f opensearch/docker-compose.yml up --build --no-start $package
+    docker cp minor-packages_${package}_1:/usr/share/opensearch/artifact/. ./
+else
+    #get url to download package
+    if [ "$version" = "DEB" ]; then
+        url=$(grep -A4 ${package}: archive_ext_packages.yml | grep -m 1 deb_url | awk '{ print $2 }')
+        filename=$(grep -A4 ${package}: archive_ext_packages.yml | grep -m 1 deb_name | awk '{ print $2 }')
+    elif [ "$version" = "RPM" ]; then
+        url=$(grep -A4 ${package}: archive_ext_packages.yml | grep -m 1 rpm_url | awk '{ print $2 }')
+        filename=$(grep -A4 ${package}: archive_ext_packages.yml | grep -m 1 rpm_name | awk '{ print $2 }')
+    fi
+
+    if [ ! -z "$url" ]; then
+        curl -o $filename -L $url;
+    else
+        echo "Error: $package not found";
+    fi
+fi
