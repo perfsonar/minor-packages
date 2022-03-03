0440b52325dbcd1f975f237ef3a7fd7f80cf2da5
Daniel Neto <daniel.jsilvan@gmail.com>
2022-03-03 19:59:07 -0300

    Temporary fix for opensearch files

--- get_archive_dependency.sh
+++ get_archive_dependency.sh
@@ -16,9 +16,14 @@ if [ "$version" != "DEB" ] && [ "$version" != "RPM" ]; then
 fi
 
 if [ "$package" = "opensearch" ] || [ "$package" = "dashboards" ]; then
-    docker-compose -p minor-packages -f opensearch/docker-compose.yml up --build --no-start $package
-    docker cp minor-packages_${package}_1:/usr/share/opensearch/artifact/. ./
-    docker-compose -p minor-packages -f opensearch/docker-compose.yml down -v
+    #docker-compose -p minor-packages -f opensearch/docker-compose.yml up --build --no-start $package
+    #docker cp minor-packages_${package}_1:/usr/share/opensearch/artifact/. ./
+    #docker-compose -p minor-packages -f opensearch/docker-compose.yml down -v
+    if [ "$package" = "opensearch" ]; then
+        curl -o opensearch-1.2.3-linux-x64.rpm -L http://monipe-central.rnp.br:8002/opensearch-1.2.3-linux-x64.rpm
+    else
+        curl -o opensearch-dashboards-1.2.0-linux-x64.rpm -L http://monipe-central.rnp.br:8002/opensearch-dashboards-1.2.0-linux-x64.rpm
+    fi
 else
     #get url to download package
     if [ "$version" = "DEB" ]; then
