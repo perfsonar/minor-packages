d67d7e29743f13020e30e4746225070982f368ff
Andy Lake <andy@es.net>
2022-06-01 13:59:28 -0400

    Adding code to pulldown opensearch packages that are too big for Git

---
+++ opensearch/README.md
@@ -0,0 +1,7 @@
+# Minor Packages
+
+Scripts to download external Opensearch packages the perfSONAR project requires but aren't available standard repos.
+These files are also too big to store in git, hence the download.
+
+Able to grab both debian and RPM packages in theory. Update the .yml file with location of desired RPM. Update the Makefile
+if you need to grab new packages.
--- opensearch/get_archive_dependency.sh
+++ opensearch/get_archive_dependency.sh
@@ -3,23 +3,23 @@
 if [ $# -ne 2 ]
   then
     echo "Illegal number of parameters"
-    echo "Usage: $0 PACKAGE DEB/RPM"
+    echo "Usage: $0 PACKAGE deb/rpm"
     exit 1
 fi
 
 package=$1
 version=$2
 
-if [ "$version" != "DEB" ] && [ "$version" != "RPM" ]; then
-    echo "Unknown OS version. Use DEB or RPM"
+if [ "$version" != "deb" ] && [ "$version" != "rpm" ]; then
+    echo "Unknown OS version. Use deb or rpm"
     exit 1
 fi
 
 #get url to download package
-if [ "$version" = "DEB" ]; then
+if [ "$version" = "deb" ]; then
     url=$(grep -A4 ${package}: archive_ext_packages.yml | grep -m 1 deb_url | awk '{ print $2 }')
     filename=$(grep -A4 ${package}: archive_ext_packages.yml | grep -m 1 deb_name | awk '{ print $2 }')
-elif [ "$version" = "RPM" ]; then
+elif [ "$version" = "rpm" ]; then
     url=$(grep -A4 ${package}: archive_ext_packages.yml | grep -m 1 rpm_url | awk '{ print $2 }')
     filename=$(grep -A4 ${package}: archive_ext_packages.yml | grep -m 1 rpm_name | awk '{ print $2 }')
 fi
